1e67734e7d294d0d835ebb49e6b390b4f121ed49
Mỹ Nguyễn <nguyen@MacBook-Pro-M1Max-32G-1T.local>
2024-11-09 11:48:17 -0800

    b4: Change inflation and remove UI calls from activity

--- app/src/main/java/com/nguyen/codelab_databinding/ui/PlainOldActivity.kt
+++ app/src/main/java/com/nguyen/codelab_databinding/ui/PlainOldActivity.kt
@@ -12,9 +12,11 @@ import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
 import androidx.core.widget.ImageViewCompat
+import androidx.databinding.DataBindingUtil
 import com.nguyen.codelab_databinding.R
 import com.nguyen.codelab_databinding.data.Popularity
 import com.nguyen.codelab_databinding.data.SimpleViewModel
+import com.nguyen.codelab_databinding.databinding.PlainActivityBinding
 
 /**
  * Plain old activity with lots of problems to fix.
@@ -27,10 +29,11 @@ class PlainOldActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        setContentView(R.layout.plain_activity)
+        val binding : PlainActivityBinding = DataBindingUtil.setContentView(this, R.layout.plain_activity)
+        binding.name = "Kamala"
+        binding.lastName = "Harris"
 
         // TODO: Explicitly setting initial values is a bad pattern. We'll fix that.
-        updateName()
         updateLikes()
     }
 
@@ -41,14 +44,6 @@ class PlainOldActivity : AppCompatActivity() {
     fun onLike(view: View) {
         viewModel.onLike()
         updateLikes()
-    }
-
-    /**
-     * So much findViewById! We'll fix that with Data Binding.
-     */
-    private fun updateName() {
-        findViewById<TextView>(R.id.plain_name).text = viewModel.name
-        findViewById<TextView>(R.id.plain_lastname).text = viewModel.lastName
     }
 
     /**
